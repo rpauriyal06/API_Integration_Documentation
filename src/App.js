abf797ffc75a2793ac04d31731b2616553d5348d
Rahul <rahul.pauriyal@wynk.in>
2021-10-05 12:20:18 +0530

    new deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         <h1>TESTING </h1>
+         <h1>TESTING NEW DEPLOYMENT</h1>
         </p>
         <a
           className="App-link"
